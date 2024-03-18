11d7a4085b5c826da331d69ce1d15f685389dd64
Aman Bharadwaj <aman.bharadwaj@razorpay.com>
2024-03-18 15:42:51 +0530

    code refactoring

--- internal/account/model.go
+++ internal/account/model.go
@@ -5,24 +5,29 @@ import (
 	"transaction-server/internal/dto"
 )
 
+// Account represents the account entity.
 type Account struct {
-	db.Model
-	Name           string `json:"name,omitempty" audit:"name"`
-	DocumentNumber string `json:"document_number,omitempty" audit:"doc_no"`
+	db.Model              // Embedding the common database model
+	Name           string `json:"name,omitempty" audit:"name"`              // Name of the account
+	DocumentNumber string `json:"document_number,omitempty" audit:"doc_no"` // Document number associated with the account
 }
 
+// TableName returns the name of the database table for the Account entity.
 func (e *Account) TableName() string {
 	return "accounts"
 }
 
+// EntityName returns the name of the entity.
 func (e *Account) EntityName() string {
 	return "account"
 }
 
+// SetDefaults sets default values for the Account entity.
 func (e *Account) SetDefaults() error {
 	return nil
 }
 
+// ToDto converts the Account entity to its DTO (data transfer object) representation.
 func (e *Account) ToDto() *dto.Account {
 	return &dto.Account{
 		ID:             e.ID,
@@ -33,6 +38,7 @@ func (e *Account) ToDto() *dto.Account {
 	}
 }
 
+// ApplyDto updates the Account entity fields based on the values provided in the DTO.
 func (e *Account) ApplyDto(val *dto.Account) {
 	e.Name = val.Name
 	e.DocumentNumber = val.DocumentNumber
--- internal/transaction/model.go
+++ internal/transaction/model.go
@@ -6,26 +6,31 @@ import (
 	"transaction-server/internal/dto"
 )
 
+// Transaction represents a financial transaction entity.
 type Transaction struct {
-	db.Model
-	AccountId     string            `json:"account_id"`
-	OperationType dto.OperationType `json:"operation_type"`
-	Amount        float64           `json:"amount"`
-	EventDate     int64             `json:"event_date"`
+	db.Model                        // Embedding the common database model
+	AccountId     string            `json:"account_id"`     // ID of the account associated with the transaction
+	OperationType dto.OperationType `json:"operation_type"` // Type of operation (e.g., purchase, withdrawal)
+	Amount        float64           `json:"amount"`         // Amount of the transaction
+	EventDate     int64             `json:"event_date"`     // Date and time when the transaction occurred (Unix timestamp)
 }
 
+// TableName returns the name of the database table for the Transaction entity.
 func (e *Transaction) TableName() string {
 	return "transactions"
 }
 
+// EntityName returns the name of the entity.
 func (e *Transaction) EntityName() string {
 	return "Transaction"
 }
 
+// SetDefaults sets default values for the Transaction entity.
 func (e *Transaction) SetDefaults() error {
 	return nil
 }
 
+// ToDto converts the Transaction entity to its DTO (data transfer object) representation.
 func (e *Transaction) ToDto() *dto.Transaction {
 	return &dto.Transaction{
 		ID:            e.ID,
@@ -36,6 +41,7 @@ func (e *Transaction) ToDto() *dto.Transaction {
 	}
 }
 
+// ApplyDto updates the Transaction entity fields based on the values provided in the DTO.
 func (e *Transaction) ApplyDto(val *dto.Transaction) {
 	e.AccountId = val.AccountID
 	e.OperationType = OperationFromString(val.OperationType)
@@ -43,6 +49,7 @@ func (e *Transaction) ApplyDto(val *dto.Transaction) {
 	e.EventDate = time.Now().Unix()
 }
 
+// OperationFromString converts a string representation of an operation type to its corresponding OperationType.
 func OperationFromString(o string) dto.OperationType {
 	for i, operationType := range dto.OperationTypes {
 		if operationType == o {
@@ -52,6 +59,7 @@ func OperationFromString(o string) dto.OperationType {
 	return 0
 }
 
+// setAmountSign sets the sign of the amount based on the operation type.
 func setAmountSign(opType dto.OperationType, amount float64) float64 {
 	if opType.IsPresent(dto.NegativeOperationTypes[:]) {
 		return -amount
@@ -59,6 +67,7 @@ func setAmountSign(opType dto.OperationType, amount float64) float64 {
 	return amount
 }
 
+// IListRequest is the interface that wraps basic request attribute getters for list requests.
 type IListRequest interface {
 	GetLimit() uint32
 	GetOffset() uint32
